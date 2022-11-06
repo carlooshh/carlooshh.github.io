be04165bf7fdce3d3c1d640dd0f47a1307e83dc8
fernando <fernando00sjp@hotmail.com>
2022-11-06 09:08:26 -0300

    corrigir a rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
   { path: 'login', component: LoginComponent },
 ];
 

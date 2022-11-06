a03d441f67928766280b2e2d91705fdf6a025485
Carlos Reis <carlos.henriquer287@gmail.com>
2022-11-06 01:38:40 -0300

    :sparkles: get risks method.

--- src/app/services/embrapa.service.ts
+++ src/app/services/embrapa.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -25,6 +25,38 @@ export class EmbrapaService {
       .toPromise()
       .then((res) => {
         console.log('GET CULTURAS', res);
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+
+  getRisks(cultureId: number, codIbge: number) {
+    let params = new HttpParams();
+
+    params = params.set('idCultura', cultureId);
+    params = params.set('codigoIBGE', codIbge);
+
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':
+          'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        Authorization: 'Bearer ' + '998e36fd-b688-3565-be2b-2f669efc1d4c',
+      },
+      params: params,
+    };
+
+    return this.http
+      .get(
+        'https://cors-anywhere.herokuapp.com/https://api.cnptia.embrapa.br/agritec/v1/zoneamento',
+        options
+      )
+      .toPromise()
+      .then((res) => {
+        console.log('GET RISKS', res);
         return res;
       })
       .catch((err) => {
